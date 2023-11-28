f97e0e917f2285715e037cb951361c839d04a26d
Asm Fahim <asmfahim1@gmail.com>
2023-11-28 21:23:37 +0600

    common loader added to utils file

--- lib/core/utils/exports.dart
+++ lib/core/utils/exports.dart
@@ -1,10 +1,9 @@
 export 'api_client.dart';
+export 'app_constants.dart';
 export 'app_error.dart';
 export 'asset_path.dart';
-export 'capital_string.dart';
 export 'colors.dart';
 export 'const.dart';
 export 'string_resource.dart';
 export 'styles.dart';
-export 'app_constants.dart';
 export 'validator.dart';

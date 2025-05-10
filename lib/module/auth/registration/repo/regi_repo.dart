b90551985bbbd1f5caf2ecda771b1f425a2d8b88
asmfahim1 <asmfahim1@gmail.com>
2025-05-10 11:26:14 +0600

    update: loginController

--- lib/module/auth/registration/repo/regi_repo.dart
+++ lib/module/auth/registration/repo/regi_repo.dart
@@ -10,26 +10,19 @@ class RegiRepo {
   RegiRepo({required this.apiClient, required this.sharedPreferences});
 
   ///file upload process
-  Future<Map<String, dynamic>> uploadFileWithDio(File file, String fileName) async {
-
-    //print('----------------${AppConstants.fileUpload}\n  ${file}\n ${fileName}============');
-
-      return await apiClient.uploadFileWithDio(AppConstants.fileUpload, file, fileName);
-
-
+  Future<Map<String, dynamic>> uploadFileWithDio(
+      File file, String fileName) async {
+    return await apiClient.uploadFile(AppConstants.fileUpload, file, fileName);
   }
-
-
 
   ///post face vectors
   Future<Response> uploadFaceVectors(Map<String, dynamic> data) async {
     return await apiClient.postData(AppConstants.postDescriptor, data);
   }
 
-
-
   ///post face vectors
-  Future<Response> uploadImagePathsToCompleteRegistration(Map<String, dynamic> data) async {
+  Future<Response> uploadImagePathsToCompleteRegistration(
+      Map<String, dynamic> data) async {
     return await apiClient.postData(AppConstants.postImagePath, data);
   }
 }
